6c342abe200038321cfd69f0960cf008e5a7a56f
danakalina <117204237+danakalina@users.noreply.github.com>
2022-11-01 20:06:12 +0200

    Sockets

---
+++ Server/Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 6667);
+            using Socket listener = new Socket(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            listener.Bind(ipEndPoint);
+            listener.Listen(100);
+
+            var handler = listener.Accept();
+            while (true)
+            {
+                // Receive message.
+                var buffer = new byte[1_024];
+                var received = handler.Receive(buffer, SocketFlags.None);
+                var response = Encoding.UTF32.GetString(buffer, 0, received);
+
+                var eom = "<|EOM|>";
+                if (response.IndexOf(eom) > -1 /* is end of message */)
+                {
+                    Console.WriteLine(
+                        $"Socket server received message: \"{response.Replace(eom, "")}\"");
+
+                    var ackMessage = "Героям слава!<|ACK|>";
+                    var echoBytes = Encoding.UTF32.GetBytes(ackMessage);
+                    handler.Send(echoBytes, 0);
+                    Console.WriteLine(
+                        $"Socket server sent acknowledgment: \"{ackMessage}\"");
+
+                    break;
+                }
+                // Sample output:
+                //    Socket server received message: "Hi friends 👋!"
+                //    Socket server sent acknowledgment: "<|ACK|>"
+            }
+        }
+    }
+}
---
+++ socket/socket/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 6667);
+            using Socket client = new Socket(
+            ipEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+            client.Connect(ipEndPoint);
+            while (true)
+            {
+                // Send message.
+                var message = "Слава Українi!<|EOM|>";
+                var messageBytes = Encoding.UTF32.GetBytes(message);
+                _ = client.Send(messageBytes, SocketFlags.None);
+                Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+                // Receive ack.
+                var buffer = new byte[1_024];
+                var received = client.Receive(buffer, SocketFlags.None);
+                var response = Encoding.UTF32.GetString(buffer, 0, received);
+                if (response.Contains("<|ACK|>"))
+                {
+                    Console.WriteLine(
+                        $"Socket client received acknowledgment: \"{response.Split("<|")[0]}\"");
+                    break;
+                }
+                // Sample output:
+                //     Socket client sent message: "Hi friends 👋!<|EOM|>"
+                //     Socket client received acknowledgment: "<|ACK|>"
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+        }
+    }
+}

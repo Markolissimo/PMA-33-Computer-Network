51fe8937612ab1666308df887d62f1dd81215eb6
Tatiana Shumylo <tanja3708@gmail.com>
2022-11-02 10:35:24 +0200

    Add post method

--- main.py
+++ main.py
@@ -1,69 +1,65 @@
 import socket
 from views import *
-# ip tcp
 
-URLS={
+URLS = {
     '/': index,
-    '/home':home,
+    '/home': home,
+    '/post': post,
 
 }
 
+
 def parse_request(request):
-    parsed=request.split(" ")
-    print(request)
-    print(parsed)
-    method=parsed[0]
-    url=parsed[1]
-    return (method,url)
+    parsed = request.split(' ')
+    method = parsed[0]
+    url = parsed[1]
+    return method, url
 
-def generate_headers(method,url):
+
+def generate_headers(method, url):
     if not method == 'GET':
-        return ('HTTP/1.1 405 Method not allowed\n\n',405)
-    if not url in URLS:
-        return ('HTTP/1.1 404 Not found\n\n',404)
-    return ('HTTp/1.1 200 Ok\n\n',200)
+        if not method == 'POST':
+                    return 'HTTP/1.1 405 Method not allowed\n\n', 405
 
-def generate_content(code,url):
-    if code==404:
+    if url in URLS:
+        return 'HTTP/1.1 200 OK\n\n', 200
+
+    return 'HTTP/1.1 404 Not found\n\n', 404
+
+
+def generate_content(code, url):
+    if code == 404:
         return '<h1>404</h1><p>Not found</p>'
-    if code==405:
+    if code == 405:
         return '<h1>405</h1><p>Method not allowed</p>'
+
     return URLS[url]()
 
-def generate_response(request):
-    print(request)
-    method,url=parse_request(request)
-    headers,code=generate_headers(method,url)
-    body=generate_content(code,url)
 
-    return (headers+body).encode()
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    body = generate_content(code, url)
+    return (headers + str(body)).encode()
+
 
 def run():
-
-    #AF сімя адресів, INET протокол ip , 4 версія
-    #tcp глобальна змінна SOCK_STREAM
-    #server_socket субєкт з протоколамими які він використовує
-    server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    #bind приймає кортеж (перший аргумент адрес, другий число порт)
-    server_socket.bind(("localhost",5000))
-    #listen перевіряє чи є вхідні пакети
-    server_socket.listen()
-
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # об’єкт сокета IPv4/TCP
+    server_socket.bind(('localhost', 5000))  # прив’язка сокета до порта сервера
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.listen()  # старт прослуховування каналу
     while True:
-        client_socket, addr=server_socket.accept()
-        request=client_socket.recv(1024)
-        print("---------------------")
+        client_socket, address = server_socket.accept()
+        request = client_socket.recv(1024)
         print(request)
         print()
-        print(addr)
+        print(address)
 
-        response=generate_response(request.decode('utf-8'))
+        response = generate_response(request.decode('UTF-8'))
 
         client_socket.sendall(response)
         client_socket.close()
 
+
 if __name__ == '__main__':
     run()
-
-
--- views.py
+++ views.py
@@ -1,7 +1,12 @@
+import json
 def index():
     with open('templates/index.html') as template:
         return template.read()
 
 def home():
     with open('templates/home.html') as template:
+        return template.read()
+
+def post():
+    with open('templates/post.html') as template:
         return template.read()
